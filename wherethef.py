e807f8687e9bb898756fef25e343ae529f86b132
Big7asty <55312568+Big7asty@users.noreply.github.com>
2020-02-07 00:54:40 -0500

    Update wherethef.py

--- wherethef.py
+++ wherethef.py
@@ -6,7 +6,7 @@ import time
 import datetime
 import configparser
 
-# Load the configuration file
+"""Load the configuration file"""
 from configparser import ConfigParser
 config = ConfigParser()
 config.read('config')
@@ -14,67 +14,95 @@ config.read('config')
 DirSearch = config.get('DirectoryLocations', 'DirSearch')
 SaveLocation = config.get('DirectoryLocations', 'SaveLocation')
 
+"""------------------------------------------------------------"""
+
+def main():
+    """Run main"""
+    splashback()
+
+    purge()
+
+    find()
+
+    moveDocs()
+
+    cpPics()
+
+    openDocs()
+
+    print()
+    print('------------------------------------------------------------------------')
+    print(' [*] If you aren\'t getting the files you expect, check the config file ')
+    print(' [*] and confirm the file paths are correct.                            ')
+    print('------------------------------------------------------------------------')
+
 def splashback():
-    print('                                                                                                                                                                    ')
-    print('                                                                                                                                                          ,#*       ')
-    print('                                                                                                                                                       @@@%&@@      ')
-    print('                                                                                                                                                #@@  /@@   *@@      ')
-    print('                                                                                                                                     %@#   @@   /@@       @@        ')
-    print('                                                                                                                          .&@         ./ @@@@#  ,@&     .@@         ')
-    print('                                                                                                                          ,@@  *%%   %@%  %@@   ,@%      ,%         ')
-    print('                                                                                                                          ,@@%&,@@@  %@%  %@@     *     #@&         ')
-    print('                                                                                                                  &@@@@@, ,@@   *@@  %@%  %@@,. %@@                 ')
-    print('                                                                                                         ./      @&       ,@@   *@@  %@%   @&*                      ')
-    print('                                                                                                 ,%@    @@       &@@@@@@% ,@@   *@@                                 ')
-    print('                                                                                           ,@@@#  @@#  %@*            @@. ,@%                                       ')
-    print('                                                                                   #& @@@@@* %@@   @@*,@#        @@@@%                                              ')
-    print('                                                                                   @@(  @@%  *@@    @@@@                                                            ')
-    print('                                                                     #@@@@@&       @@   @@%  *@@    *@@,                                                            ')
-    print('                                                             #@@@.  @@,            @@   @@%  *@#    @@*                                                             ')
-    print('                                                     , @@  @@   @@, #@@@@@@@       @@   *.        @@%                                                               ')
-    print('                                *%          .#@@/  ,@@&   @@@@@&*    .   (@#                                                                                        ')
-    print('                           .%@ %@@   ,.   ,@#  (@@ ,@@.   @@#   @@  &@@@%.                                                                                          ')
-    print('                    *%@*   @@# %@@&@@@@# ,@@%@@@@% ,@@    /@@@@@*                                                                                                   ')
-    print('             .&@,   @@@@   @@  %@@   @@% #@@    ,# ,@@                                                                                                              ')
-    print('              %@&  .@%%@# (@%  %@@   @@%  @@@%@@*                                                                                                                   ')
-    print('               @@, %@,.@@ @@.  %@@   @@%                                                                                                                            ')
-    print('               %@@.@%  @@%@%   %@@                                                                                                                                  ')
-    print('                @@@@.  ,@@@                                                                                                                                         ')
-    print('                %@@&                                                                                                                                                ')
-    print('                                                                                                                                                                    ')
-    print('                                                                                                                                                                    ')
-    print('                                                                                      A Big7asty original                                                           ')
-    print('                                                        Have you been saving everything you need within the directory you are searching?                            ')
+    """Cool splashback"""
+    print("""                                                                                                                                                                  
+                                                                                                                                                                     ,#*       
+                                                                                                                                                                  @@@%&@@      
+                                                                                                                                                           #@@  /@@   *@@      
+                                                                                                                                                %@#   @@   /@@       @@        
+                                                                                                                                     .&@         ./ @@@@#  ,@&     .@@         
+                                                                                                                                     ,@@  *%%   %@%  %@@   ,@%      ,%         
+                                                                                                                                     ,@@%&,@@@  %@%  %@@     *     #@&         
+                                                                                                                             &@@@@@, ,@@   *@@  %@%  %@@,. %@@                 
+                                                                                                                    ./      @&       ,@@   *@@  %@%   @&*                      
+                                                                                                            ,%@    @@       &@@@@@@% ,@@   *@@                                 
+                                                                                                      ,@@@#  @@#  %@*            @@. ,@%                                       
+                                                                                              #& @@@@@* %@@   @@*,@#        @@@@%                                              
+                                                                                              @@(  @@%  *@@    @@@@                                                            
+                                                                                #@@@@@&       @@   @@%  *@@    *@@,                                                            
+                                                                        #@@@.  @@,            @@   @@%  *@#    @@*                                                             
+                                                                , @@  @@   @@, #@@@@@@@       @@   *.        @@%                                                               
+                                           *%          .#@@/  ,@@&   @@@@@&*    .   (@#                                                                                        
+                                      .%@ %@@   ,.   ,@#  (@@ ,@@.   @@#   @@  &@@@%.                                                                                          
+                               *%@*   @@# %@@&@@@@# ,@@%@@@@% ,@@    /@@@@@*                                                                                                   
+                        .&@,   @@@@   @@  %@@   @@% #@@    ,# ,@@                                                                                                              
+                         %@&  .@%%@# (@%  %@@   @@%  @@@%@@*                                                                                                                   
+                          @@, %@,.@@ @@.  %@@   @@%                                                                                                                            
+                          %@@.@%  @@%@%   %@@                                                                                                                                  
+                           @@@@.  ,@@@                                                                                                                                         
+                           %@@&                                                                                                                                                
+                                                                                                                                                                               
+                                                                                                                                                                               
+                                                                                                 A Big7asty original                                                           
+                                                                   Have you been saving everything you need within the directory you are searching?                            """)
     return 0
 
 def purge():
-    #Purges the working directory
+    """Purges the working directory"""
     print()
-    print(' [!] Do you need any of the files in transfer_dir? (They will be deleted!)')
-    print()
-    rm_files = str(input(' [+] y or n: '))
+    print('The system time is:')
+    date = 'date'
+    print('-------------------------------')
+    os.system(date)
+    print('-------------------------------')
+    print('  [!] NOTE! If the system clock is out, files you are searching would have been saved against this time.\n')
+    print('  [!] Do you need any of the files in transfer_dir? (They will be deleted!)\n')
+    rm_files = str(input('  [+] y or n: ')).lower()
     if rm_files == "y":
-        print(' [!] QUITTING')
+        print('  [!] QUITTING')
         sys.exit()
     elif rm_files == "n":
         clean_up = 'rm -rf transfer_dir/*'
         os.system(clean_up)
     else:
-        print(' [!] Invalid input, QUITTING')
+        print('  [!] Invalid input, QUITTING')
         sys.exit()
     return 0
 
 def find():
-    #Finds all the files saved from within a particular timeframe
+    """Finds all the files saved from within a particular timeframe"""
     print()
-    print(' [*] Start date?')
+    print('  [*] Start date?')
     print()
-    print(' [*] i.e. 2001230900 for 9am on the 23rd of January, 2020')
+    print('  [*] i.e. 2001230900 for 9am on the 23rd of January, 2020')
     from_date = input()
     print()
-    print(' [*] End date?')
+    print('  [*] End date?')
     print()
-    print(' [*] i.e. 2001231700 for 5pm on the 23rd of January, 2020')
+    print('  [*] i.e. 2001231700 for 5pm on the 23rd of January, 2020')
     to_date = input()
     find_start = 'touch -t ' + str(from_date) + ' /tmp/t1'
     find_end = 'touch -t ' + str(to_date) + ' /tmp/t2'
@@ -85,10 +113,10 @@ def find():
     return 0
 
 def moveDocs():
-    #Brings the files listed in /tmp/ to the pwd
+    """Brings the files listed in /tmp/ to the pwd"""
     working_file = 'cat /tmp/transfer > yourfiles.txt'
     os.system(working_file)
-    #Reads the listed file paths and consolidates them into one directory
+    """Reads the listed file paths and consolidates them into one directory"""
     with open('yourfiles.txt', 'r+') as f:
         contents = [x.strip() for x in f.readlines()]
         counter = 0
@@ -100,35 +128,20 @@ def moveDocs():
     return 0
 
 def cpPics():
-    #Copy's screenshot's default format .png files from /root/Documents/05_fieldwork/screenshots/ and /root/Pictures/
-    cp_pics1 = 'cp ' + str(DirSearch) + '/05_fieldwork/screenshots/*.png ' + str(SaveLocation) + '/wherethef-master/transfer_dir > /dev/null 2>&1'
+    """Copy's screenshot's default format .png files from /root/Documents/05_fieldwork/screenshots/ and /root/Pictures/"""
+    cp_pics1 = 'cp ' + str(DirSearch) + '/*.png ' + str(SaveLocation) + '/wherethef-master/transfer_dir > /dev/null 2>&1'
     cp_pics2 = 'cp /root/Pictures/*.png ' + str(SaveLocation) + '/wherethef-master/transfer_dir > /dev/null 2>&1'
     os.system(cp_pics1)
     os.system(cp_pics2)
     return 0
 
 def openDocs():
-    #Clean up pwd and opens File Manager for transfer
+    """Clean up pwd and opens File Manager for transfer"""
     clean_up = 'rm yourfiles.txt'
     os.system(clean_up)
     give_me_my_files = 'xdg-open transfer_dir'
     os.system(give_me_my_files)
     return 0
 
-splashback()
-
-purge()
-
-find()
-
-moveDocs()
-
-cpPics()
-
-openDocs()
-
-print()
-print('------------------------------------------------------------------------')
-print(' [*] If you aren\'t getting the files you expect, check the config file ')
-print(' [*] and confirm the file paths are correct.                            ')
-print('------------------------------------------------------------------------')
+if __name__ == "__main__":
+    main()
